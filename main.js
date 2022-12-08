f89c4c586e77d41464613b7613ef9766a2e298c9
Abdulrahman <65297902+Abood10s@users.noreply.github.com>
2022-12-08 08:12:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -61,12 +61,7 @@ start.addEventListener("click", () => {
         let seconds = time % 60;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         timer.textContent = `${minutes}m : ${seconds}s `;
-        if (time !== 00 && seconds !== 00) {
-      time--;
-    } else {
-      timer.style.color = "red";
-      alert("timer Ended!");
-    }
+     
       }
       interval = setInterval(updateTimer, 1000);
     } else {
